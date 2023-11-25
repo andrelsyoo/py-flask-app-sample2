82af8fd3649f73d0850d9211f5ad300451ca2365
Andre LS <aluisdossantos@pinterest.com>
2023-11-25 18:49:07 -0300

    feat(update): tf

--- config/terraform/backend.tf
+++ config/terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "playground-tfstate23"
     key            = "<%= expansion('terraspace/:ENV-:TS_APP-:REGION-:MOD_NAME/terraform.tfstate') %>"
-    region         = "us-east-2"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "ecsforapp-tfstate23-lock"
   }
