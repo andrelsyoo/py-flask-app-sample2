b1510bd57e1ad6fabcb173ec5f870645285244d7
Andre LS <aluisdossantos@pinterest.com>
2023-11-25 12:17:02 -0300

    feat(updates): adding more routes+requirements

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, jsonify, request
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Route for "/hello"
 @app.route('/hello')
 def hello():
-    response_data = {"message": "Hello, World!"}
+    response_data = {"message": "Hello World!"}
     return jsonify(response_data), 200
+
+@app.route('/current_time')
+def current_time():
+    name = request.args.get('name', 'Guest')
+    timestamp = int(datetime.timestamp(datetime.now()))
+    message = f"Hello {name}"
+    response_data = {
+        "timestamp": timestamp,
+        "message": message
+    }
+    return jsonify(response_data), 200
+
+@app.route('/healthcheck')
+def healthcheck():
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
